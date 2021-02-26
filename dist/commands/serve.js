82fe462a437c9223210f0f2dc8ed10f1313f1c39
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-02-26 16:46:33 +0100

    successfully receiving port & filename

--- dist/commands/serve.js
+++ dist/commands/serve.js
@@ -1,14 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serveCommand = void 0;
-//importing command
+//importing command & serve
 var commander_1 = require("commander");
+var local_api_1 = require("local-api");
 //serve command
 exports.serveCommand = new commander_1.Command()
     .command('serve [filename]')
     .description('Open a file for editing')
     .option('-p, --port <number>', 'port to run server on', '4005')
-    .action(function (filename, options) {
+    .action(function (filename, _a) {
     if (filename === void 0) { filename = 'notebook.js'; }
-    console.log(filename, options);
+    var port = _a.port;
+    local_api_1.serve(parseInt(port), filename, '/');
 });
