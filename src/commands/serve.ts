7b1128601bc3b6485fcc7a23a4e6ff47c075c781
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-02-26 17:09:52 +0100

    handling specific dir/files

--- src/commands/serve.ts
+++ src/commands/serve.ts
@@ -1,4 +1,5 @@
-//importing command & serve
+//importing command & serve & path
+import path from 'path';
 import { Command } from 'commander';
 import { serve } from 'local-api';
 //serve command
@@ -7,5 +8,7 @@ export const serveCommand = new Command()
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run server on', '4005')
   .action((filename = 'notebook.js', { port }: { port: string }) => {
-    serve(parseInt(port), filename, '/');
+    //getting file inside folder if user specifies one
+    const dir = path.join(process.cwd(), path.dirname(filename));
+    serve(parseInt(port), path.basename(filename), dir);
   });
