669a7ff1c7456ce74047769803dff54ad676631c
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-02-26 16:33:54 +0100

    adding commands to cli

---
+++ dist/commands/serve.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.serveCommand = void 0;
+//importing command
+var commander_1 = require("commander");
+//serve command
+exports.serveCommand = new commander_1.Command()
+    .command('serve [filename]')
+    .description('Open a file for editing')
+    .option('-p, --port <number>', 'port to run server on', '4005')
+    .action(function (filename, options) {
+    if (filename === void 0) { filename = 'notebook.js'; }
+    console.log(filename, options);
+});
