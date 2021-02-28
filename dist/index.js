3abfbf16f289a754c2e38bdb4cbab3d79c1f13d4
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-02-28 21:01:25 +0100

    Delete dist directory

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//importing commands
-var commander_1 = require("commander");
-var serve_1 = require("./commands/serve");
-//executing
-commander_1.program.addCommand(serve_1.serveCommand);
-commander_1.program.parse(process.argv);
